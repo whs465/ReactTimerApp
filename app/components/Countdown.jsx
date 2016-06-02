4e2f90db59afc7dab68be4ef37fca836fa80fd51
William Hernandez Sierra <whs465@gmail.com>
2016-06-02 18:03:03 +0630

    add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Countdown</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function () {
+      return (
+        <div>
+          <h1>Timer</h1>
+        </div>
+      );
+    }
+});
+
+module.exports = Timer;
