d7c5d192093462b32f42565e56d24a7da8bfe282
William Hernandez Sierra <whs465@gmail.com>
2016-06-04 12:17:45 +0630

    support Foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -5,8 +5,6 @@ var Main = require('Main');
 var Timer = require('Timer');
 var Countdown = require('Countdown');
 
-// load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
